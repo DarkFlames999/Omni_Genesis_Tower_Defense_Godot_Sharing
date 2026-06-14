f0d59999652fa625c4d8bb8b3c82511a45eaceb3
DarkFlames999 <76727518+DarkFlames999@users.noreply.github.com>
2026-06-13 23:21:20 -0700

    Updated meEnu functionality/settings template

---
+++ Scenes/UI/main_menu.gd
@@ -0,0 +1,15 @@
+extends Control
+
+func _ready():
+	%PLAY.pressed.connect(play)
+	%SETTINGS.pressed.connect(settings) 
+	%QUIT.pressed.connect(quit_game)
+	
+func play():
+	get_tree().change_scene_to_file("res://Scenes/Game/main_game.tscn")
+	
+func settings():
+	get_tree().change_scene_to_file("res://Scenes/UI/Settings_menu.tscn")
+	
+func quit_game():
+	get_tree().quit()
---
+++ Scenes/UI/settings_menu.gd
@@ -0,0 +1,19 @@
+extends Control
+
+##More or less a test script
+func _ready() -> void:
+	%CheckButton.pressed.connect(_Check_Button_1)
+	%CheckButton2.pressed.connect(_Check_Button_2)
+	%CheckButton3.pressed.connect(_Check_Button_3)
+	
+func _Check_Button_1() -> void:
+	%Label.label_settings.outline_size = 4
+	%Label.label_settings.outline_color = Color.PURPLE
+	
+func _Check_Button_2() -> void:
+	%Label2.label_settings.outline_size = 3
+	%Label2.label_settings.outline_color = Color.RED
+	
+func _Check_Button_3() -> void:
+	%Label3.label_settings.outline_size = 2
+	%Label3.label_settings.outline_color = Color.AQUA
